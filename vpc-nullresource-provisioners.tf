10ee83376f80a7593947aefe4df03f84b6d0c59b
Haris <harisgujjar317@gmail.com>
2023-02-16 20:37:54 -0500

    finall

---
+++ vpc-nullresource-provisioners.tf
@@ -0,0 +1,22 @@
+resource "null_resource" "name" {
+    depends_on = [ module.ec2_instance_bastionHost ]
+    connection {
+        type     = "ssh"
+        user     = "ec2-user"
+        private_key = file("private-key/terraform-key.pem")
+        host     = aws_eip.vpc_elastic.public_ip
+    }
+    # Copies the myapp.conf file to /etc/myapp.conf
+    provisioner "file" {
+        source      = "private-key/terraform-key.pem"
+        destination = "/tmp/terraform-key.pem"
+    }
+      provisioner "remote-exec" {
+        inline = [ "sudo chmod +400 /tmp/terraform-key.pem" ]
+    }
+    provisioner "local-exec" {
+        command = "echo VPC created on `date` and VPC ID: ${module.vpc.vpc_id} >> creation-time-vpc-id.txt"
+        working_dir = "local-exec-output-files/"
+        #on_failure = continue
+    }
+}
